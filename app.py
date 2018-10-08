150e3425d3198b9794d7ac8dab7faaff344fb454
Goodman <GoodmanClint@praintl.com>
2018-10-07 23:08:29 -0500

    Working Copy - Update 3

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ app = Flask(__name__)
 #################################################
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL','') or "sqlite:///db/bellybutton.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 #from .model import Sample, Sample_Metadata
